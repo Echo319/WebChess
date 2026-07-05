ce6799855bf27a0f246d216804c4535ae3015991
Robin Davies <r.davies319@gmail.com>
2026-07-05 16:57:43 +0100

    Migrated to Spring Websockets

--- ChessServer/src/main/java/com/rdavies/Main.java
+++ ChessServer/src/main/java/com/rdavies/Main.java
@@ -1,7 +1,11 @@
 package com.rdavies;
 
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Main {
+
     public static void main(String[] args) {
 
         // open websocket
