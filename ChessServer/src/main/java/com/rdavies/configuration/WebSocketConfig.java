29674575f9a490fc5aa52c92a8b31133b83d1753
Robin Davies <r.davies319@gmail.com>
2026-07-05 16:57:56 +0100

    Added socket config

---
+++ ChessServer/src/main/java/com/rdavies/configuration/WebSocketConfig.java
@@ -0,0 +1,28 @@
+package com.rdavies.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        // Enables a simple in-memory broker to carry the moves back to the client
+        config.enableSimpleBroker("/topic");
+        // Prefix for messages bound for methods annotated with @MessageMapping
+        config.setApplicationDestinationPrefixes("/app");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        // The endpoint where clients connect
+        registry.addEndpoint("/game")
+                .setAllowedOriginPatterns("*") // Configure safely for production
+                .withSockJS(); // Fallback for browsers that don't support WebSockets natively
+    }
+}
